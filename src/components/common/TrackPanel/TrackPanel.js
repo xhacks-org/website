4df1d24e8ea8906cc914300cb5a961c8b0552ef5
Harshith Latchupatula <harshithl1777@gmail.com>
2021-05-20 20:30:14 -0400

    finished tracks section responsive (100%)

--- src/components/common/TrackPanel/TrackPanel.js
+++ src/components/common/TrackPanel/TrackPanel.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import panelBackground from './assets/panelBackground.svg';
 import './trackpanel.css';
 
-const TrackPanel = ({ name, desc, icon }) => {
+const TrackPanel = ({ name, desc, icon, featured }) => {
+
     return (
         <div className='track-panel'>
+            {featured && <label className='panel-featured'>FEATURED</label>}
             <div className='panel-content'>
-                
+                <img alt='track icon' src={icon} className={(featured) ? 'panel-icon-featured' : 'panel-icon'} />
+                <h1 className='panel-name'>{name}</h1>
+                <p className='panel-desc'>{desc}</p>
             </div>
-            <img src={panelBackground} alt='panel border' className='panel-bg' />        
         </div>
     );
 }
--- src/containers/TrackSection/TrackSection.js
+++ src/containers/TrackSection/TrackSection.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import TrackPanel from '../../components/common/TrackPanel/TrackPanel';
-import { data } from './assets/tracks.json';
-import styles from './tracksection.css';
+import assets from './assets/tracks.json';
+import idea from './assets/idea.svg';
+import rookie from './assets/rookie.svg';
+import relay from './assets/relay.svg';
+import veteran from './assets/veteran.svg';
+import solo from './assets/solo.svg';
+import scale from './assets/scale.svg';
+import './tracksection.css';
 
 const TrackSection = ({ reference }) => {
-
-    const renderTracks = () => {
-        return data.map(({ name, desc, icon, featured }) => {
-            return (
-                <TrackPanel name={name} desc={desc} icon={icon} featured={featured} />
-            );
-        });
-    }
+    const imageAssets = { idea, rookie, relay, veteran, solo, scale };
 
     return (
         <section ref={reference} className='track-section'>
@@ -20,7 +19,11 @@ const TrackSection = ({ reference }) => {
             </div>
             <p className='track-desc'>We’ve been to hackathons with only a couple of tracks and while they’re not bad, we think more tracks is generally more inclusive for everyone. That’s why we’re doing our best to have a couple tracks for everyone, so that you have multiple chances to learn and win! </p>
             <div className='track-grid'>
-                <TrackPanel />
+                {assets.data.map(({ name, desc, icon, featured }) => {
+                    return (
+                        <TrackPanel key={name} name={name} desc={desc} icon={imageAssets[icon]} featured={featured} />
+                    );
+                })}
             </div>
         </section>
     );
