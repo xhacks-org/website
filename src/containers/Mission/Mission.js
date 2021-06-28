737ffd7ad91ad60716b0960780c055455fd551ef
Harshith Latchupatula <harshithl1777@gmail.com>
2021-06-28 15:40:05 -0400

    updated mission

--- src/containers/Mission/Mission.js
+++ src/containers/Mission/Mission.js
@@ -11,15 +11,13 @@ const Mission = ({ reference }) => {
             <div className='mission__content'>
                 <h1 className="content__title maintext-gradient">Our Mission</h1>
                 <img className="content__whatWeDo" src={topPic} alt='what we do' />
-                <h1 className="content__text">Let’s be honest. Hackathons are great.
-                 But like all great things, we think that they can be improved even further.
-                  As avid programmers ourselves, we’ve been to many hackathons and we’ve noticed
+                <h1 className="content__text">As avid programmers ourselves, we’ve been to many hackathons and we’ve noticed
                    one common trend - after the hackathon, most projects get abandoned and during
                     the hackathon, most projects are made simply for prizes.
                         <br></br><br></br>
                         That’s why we made XHacks. Although we know that a full production app can't be 
                         built in one weekend we'll do our best to ensure that you have the necessary skills, 
-                        connections and resources to get started on a product that can scale beyond the hackathon.</h1>
+                        connections and resources to get started on a product that can scale beyond the hackathon. Additionally, we provide prizes and workshops that truly help further our hackers' skills far more than your regular event.</h1>
                 <img className="content__loveTech" src={bottomLeft} alt='love tech'/>
                 <img className="content__yay" src={bottomRight} alt='yay'/>
             </div>
